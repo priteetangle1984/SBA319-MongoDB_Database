624cebcf03b55e87c784f2a9949ba892c603c369
PriteeTangle <140731865+priteetangle1984@users.noreply.github.com>
2024-04-08 11:38:34 -0400

    updated

---
+++ controllers/laptop.mjs
@@ -0,0 +1,5 @@
+import express from 'express';
+const router = express.Router();
+import Laptop from '../models/laptop.mjs'
+import db from '../db/conn.mjs'
+
---
+++ index.mjs
@@ -0,0 +1,34 @@
+import exxpress from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+import jsxviewEngine from "jsx-view-engine";
+import methodOverride from "method-override";
+import db from "./db/conn.mjs";
+import laptopRoutes from "./controllers/laptop.mjs";
+import phoneRoutes from "./controllers/phone.mjs";
+import machineRoutes from "./controllers/machine.mjs";
+
+//creating express application and other variables
+const app = express();
+const PORT = process.env.PORT || 5051;
+
+// MIDDLEWARE
+
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('method'));
+
+//SET UP VIEW ENGINE
+
+app.set('view engine', 'jsx');
+app.set('views', './views');
+app.engine('jsx', jsxviewEngine());
+
+
+//ROUTES
+app.use("/laptops", laptopRoutes);
+app.get('/', (req, res) => {
+  res.send(
+    `<div> Root routes for laptops, phones, and machines <br /> <a href='/laptops'>Laptops</a><br/><a href='phones'>Phones<br/><a href='phones'>Machines</div> `
+  )
+})
